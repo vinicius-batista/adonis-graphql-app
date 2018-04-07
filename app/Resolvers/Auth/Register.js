69c8ba813c6d43f6bed99684d6b86f4a6f77ee09
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-04-07 20:20:03 -0300

    refactor register user

--- app/Resolvers/Auth/Register.js
+++ app/Resolvers/Auth/Register.js
@@ -5,16 +5,18 @@ const { validateAll } = use('Validator')
 const GraphQLError = use('Adonis/Addons/GraphQLError')
 const User = use('App/Models/User')
 
+const createUserAfterValidation = input => validation => {
+  if (validation.fails()) {
+    throw new GraphQLError('Validation Failed', validation.messages())
+  }
+
+  const { passwordConfirm, ...userData } = input
+  return User.create(userData)
+}
+
 const registerUser = (_, { input }) =>
   validateAll({ ...input }, registerRules)
-    .then(validation => {
-      if (validation.fails()) {
-        throw new GraphQLError('Validation Failed', validation.messages())
-      }
-
-      const { passwordConfirm, ...userData } = input
-      return User.create(userData)
-    })
+    .then(createUserAfterValidation(input))
     .then(() => 'User registred successfully.')
 
 module.exports = {
