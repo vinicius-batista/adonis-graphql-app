0ac66dbe8858b4c53834dcf906f9873f2f2e4e11
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-04-07 11:06:26 -0300

    add graphql

--- start/routes.js
+++ start/routes.js
@@ -1,20 +1,13 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URL's and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.0/routing
-|
-*/
-
 const Route = use('Route')
+const GraphQLServer = use('Adonis/Addons/GraphQLServer')
 
-Route.get('/', ({ request }) => {
-  return { greeting: 'Hello world in JSON' }
-})
+const options = {
+  chacheControl: true,
+  debug: false
+}
+
+const handle = (context) => GraphQLServer.handle(context, options)
+
+Route.route('/graphql', handle, ['GET', 'POST'])
